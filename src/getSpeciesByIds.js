100ee5c69830a36ddbc7190bb923be39258bdc79
DanielFerreira <daniel18mtm@gmail.com>
2023-07-19 09:57:01 -0300

    Iniciando

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (ids, ida, ido, ide, idi) => {
+  const dados = data.species
+    .filter((objeto) => objeto.id === ids
+  || objeto.id === ida || objeto.id === ido
+  || objeto.id === ide || objeto.id === idi);
+  return dados.map((objeto) => objeto.residents);
 };
+// const getSpeciesByIds = (ids, ida, ido, ide, idi) => {
+//   const arr = [];
+//   const dados = data.species;
+//   for(let dado of dados) {
+//   if(dado.id === ids || dado.id === ida
+//   || dado.id === ido
+//   || dado.id === ide || dado.id === idi){
+//    arr.push(dado.residents);
+//     }
+//    }
+//   };
 
 module.exports = getSpeciesByIds;
