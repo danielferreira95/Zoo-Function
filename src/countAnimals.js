0e41850149f42c71b02b342aaae75d3bd2e0f3ca
DanielFerreira <daniel18mtm@gmail.com>
2023-07-20 08:34:38 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,6 +1,22 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = () => {
+const { species } = data;
 
+const countAnimals = (animal) => {
+  if (animal === undefined) {
+    const conta = species.map((objeto) => ({ [objeto.name]: objeto.residents.length }));
+    const obj = Object.assign(...conta);
+    return obj;
+  }
+  const chaves = Object.keys(animal);
+  const valores = Object.values(animal);
+  const filtrarNome = (elementoAnimal) => elementoAnimal.name === valores[0];
+  const filtrarSexo = (elementoAnimal) => elementoAnimal.sex === valores[1];
+  const conjuntoAnimais = species.find(filtrarNome).residents;
+  if (chaves.includes('sex')) {
+    return conjuntoAnimais.filter(filtrarSexo).length;
+  }
+  return conjuntoAnimais.length;
 };
+
 module.exports = countAnimals;
