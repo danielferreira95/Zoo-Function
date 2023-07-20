5d24b6d0adce03f7b630b6148e07477287fd0e3f
DanielFerreira <daniel18mtm@gmail.com>
2023-07-20 10:16:14 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const filtrarCrianca = (entrant) => entrant.age < 18;
+  const contaCrianca = entrants.filter(filtrarCrianca).length;
+  const filtrarAdulto = (entrant) => entrant.age >= 18 && entrant.age < 50;
+  const contaAdulto = entrants.filter(filtrarAdulto).length;
+  const filtrarSenhor = (entrant) => entrant.age >= 50;
+  const contaSenhor = entrants.filter(filtrarSenhor).length;
+  const obj = (adulto, crianca, senhor) => ({
+    adulto,
+    crianca,
+    senhor,
+  });
+  return obj(contaAdulto, contaCrianca, contaSenhor);
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants !== undefined && entrants.length !== undefined) {
+    const pessoas = countEntrants(entrants);
+    const chaves = Object.keys(pessoas);
+    const total = chaves
+      .reduce((acumulador, indice) => acumulador + pessoas[indice] * data.prices[indice], 0);
+    return total;
+  } return 0;
 };
 
 module.exports = { calculateEntry, countEntrants };
